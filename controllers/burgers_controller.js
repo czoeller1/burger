8c776dde98b0cfb6ba465324e01860f9c5a45c8e
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-28 20:38:15 -0700

    added routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const burgers = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get("/index", (req, res) => {
+  burgers.selectAll((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burgers.insertOne(
+    ["name", "devoured"],
+    [req.body.name, req.body.devoured],
+    (result) => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  burgers.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
