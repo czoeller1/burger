6e23ca2d61d0e4d3346b48432108f86af40b5485
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-28 20:12:43 -0700

    Made schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NULL,
+    devoured BOOLEAN NULL,
+):
